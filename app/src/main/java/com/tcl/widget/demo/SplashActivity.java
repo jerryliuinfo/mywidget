5408681546ed804dbb19223f6bc44d6df0038257
xiangxiang.liu <liuxiangxiang1234@163.com>
2016-10-10 23:32:47 +0800

    增加贝塞尔曲线练习

--- app/src/main/java/com/tcl/widget/demo/SplashActivity.java
+++ app/src/main/java/com/tcl/widget/demo/SplashActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
 import com.tcl.widget.demo.container.FragmentContainerActivity;
+import com.tcl.widget.demo.widget.PathBooleanFragment;
 
 /**
  * @author Jerry
@@ -25,7 +26,8 @@ public class SplashActivity extends AppCompatActivity {
         //RadarActivity.launch(this);
         //FragmentContainerActivity.launch(this,RadarFragment.class, null);
         //FragmentContainerActivity.launch(this,BeizerFragment.class, null);
-        FragmentContainerActivity.launch(this,BeizerFragment2.class, null);
+        //FragmentContainerActivity.launch(this,BeizerFragment2.class, null);
+        FragmentContainerActivity.launch(this,PathBooleanFragment.class, null);
         finish();
     }
 }
